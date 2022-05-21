f2401d156a1e157e97d0850fcac63b8d9bc9872c
izzy417 <israeldail2@gmail.com>
2022-05-21 00:31:26 +0000

    different extensions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let pronoun = ['the','our','their'];
 let adj = ['great', 'big','huge','small','kindofsmall' ];
 let noun = ['jogger','racoon','runner','cop','bus'];
+let extensions = ['.us','.net','.corp','.co']
 
 for(let i=0; i<pronoun.length; i++){
 
@@ -8,7 +9,11 @@ for(let i=0; i<pronoun.length; i++){
         
         for(let k=0; k<noun.length; k++){
 
-            console.log(pronoun[i]+adj[j]+noun[k] + '.com')
+            for(let e=0; e<extensions.length; e++){
+
+                console.log(pronoun[i]+adj[j]+noun[k]+extensions[e])
+            }
+            
   
         }
 
