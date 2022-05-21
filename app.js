f72f106f7f7f0648c197ddb7a0c6e57b3dd394c1
izzy417 <israeldail2@gmail.com>
2022-05-21 00:16:31 +0000

    automated domain generator complete

---
+++ app.js
@@ -0,0 +1,24 @@
+let pronoun = ['the','our','their'];
+let adj = ['great', 'big','huge','small','kindofsmall' ];
+let noun = ['jogger','racoon','runner','cop','bus'];
+
+for(let i=0; i<pronoun.length; i++){
+
+    for(let j=0; j<adj.length; j++ ){
+        
+        for(let k=0; k<noun.length; k++){
+
+            console.log(pronoun[i]+adj[j]+noun[k] + '.com')
+  
+        }
+
+  
+    }
+
+    
+    
+
+}
+
+ 
+
